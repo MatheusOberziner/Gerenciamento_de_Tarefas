a53158cc51063081debeb04b80f549a2ccf17235
Matheus Schmidt Oberziner <matheus.oberziner@unidavi.edu.br>
2023-11-13 20:47:00 -0300

    Iniciado o desenvolvimento

---
+++ src/components/TaskCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <q-card flat bordered>
+    <q-item :v-if="task" clickable v-ripple>
+      <q-item-section avatar>
+        <q-avatar>
+          <q-icon :name="task.completed ? 'check' : 'alarm'"></q-icon>
+        </q-avatar>
+      </q-item-section>
+      <q-item-section>
+        <q-item-label>
+          {{ task.description }}
+        </q-item-label>
+        <q-item-label caption lines="1">
+          {{ task.completed ? 'Concluída' : 'Pendente' }}
+        </q-item-label>
+      </q-item-section>
+      <q-item-section side top>
+        <q-btn @click="toggleTask" color="primary" :icon="task.completed ? 'alarm' : 'check'">
+          {{ task.completed ? 'Reabrir' : 'Concluir' }}
+        </q-btn>
+        <q-btn @click="deleteTask" color="negative" icon="delete"></q-btn>
+      </q-item-section>
+    </q-item>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'TaskCard',
+  props: {
+    task: {
+      type: Object,
+      required: true
+    },
+    onDelete: {
+      type: Function,
+      required: true
+    },
+    onToggle: {
+      type: Function,
+      required: true
+    }
+  },
+  methods: {
+    deleteTask () {
+      this.onDelete(this.task)
+    },
+    toggleTask () {
+      this.onToggle(this.task)
+    }
+  }
+}
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,95 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+  <q-page style="background-color: #e7ecf1;">
+    <q-tabs
+      v-model="activeTab"
+      dense
+      class="q-mb-md"
+      active-color="positive"
+      indicator-color="positive"
     >
+      <q-tab name="pending">Pendentes</q-tab>
+      <q-tab name="inProgress">Em Andamento</q-tab>
+      <q-tab name="completed">Concluídas</q-tab>
+    </q-tabs>
+
+    <q-btn @click="addTask" icon="add" color="positive" />
+
+    <q-page-section>
+      <q-tab-panels v-model="activeTab" animated>
+        <q-tab-panel name="pending">
+          <TaskCard
+            v-for="task in getTasksByStatus('pending')"
+            :key="task.id"
+            :task="task"
+            :onToggle="toggleTask"
+            :onDelete="deleteTask"
+          />
+        </q-tab-panel>
+
+        <q-tab-panel name="inProgress">
+          <TaskCard
+            v-for="task in getTasksByStatus('inProgress')"
+            :key="task.id"
+            :task="task"
+            :onToggle="toggleTask"
+            :onDelete="deleteTask"
+          />
+        </q-tab-panel>
+
+        <q-tab-panel name="completed">
+          <TaskCard
+            v-for="task in getTasksByStatus('completed')"
+            :key="task.id"
+            :task="task"
+            :onToggle="toggleTask"
+            :onDelete="deleteTask"
+          />
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-page-section>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'IndexPage'
-})
+import TaskCard from '../components/TaskCard.vue'
+export default {
+  name: 'IndexPage',
+  components: {
+    TaskCard
+  },
+  data () {
+    return {
+      tasks: [
+        { id: 1, description: 'Tarefa 1', status: 'pending' },
+        { id: 2, description: 'Tarefa 2', status: 'inProgress' },
+        { id: 3, description: 'Tarefa 3', status: 'completed' }
+      ],
+      activeTab: 'pending'
+    }
+  },
+  computed: {
+    getTasksByStatus () {
+      return status => this.tasks.filter(task => task.status === status)
+    }
+  },
+  methods: {
+    addTask () {
+      const newTask = {
+        id: this.tasks.length + 1,
+        description: `Tarefa ${this.tasks.length + 1}`,
+        status: 'pending'
+      }
+      this.tasks.push(newTask)
+    },
+    toggleTask (task) {
+      task.status = task.status === 'pending' ? 'completed' : 'pending'
+    },
+    deleteTask (task) {
+      const taskIndex = this.tasks.indexOf(task)
+      if (taskIndex !== -1) {
+        this.tasks.splice(taskIndex, 1)
+      }
+    }
+  }
+}
 </script>
